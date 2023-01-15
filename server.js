33045166c4b65e349a2e6f4a3ecc821b340d10e3
Jey Ashokkumaar <jeyashokkumaar@gmail.com>
2023-01-15 11:15:34 +0000

    Names and quotes submitted into the form are now stored in the MongoDB database

--- server.js
+++ server.js
@@ -9,22 +9,26 @@ MongoClient.connect(url, {useUnifiedTopology: true}, (err,client) => {
     if (err) return console.error(err)
     console.log('Connected to Database')
     const db = client.db('star-wars-quotes')
-    app.use()
-    app.get()
-    app.post()
-    app.listen()
+    const quotesCollection = db.collection('quotes')
+
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.listen(3000, function() { // Creates server
+        console.log('listening on 3000')
+    });
+    app.get('/', (req,res) => {
+        res.sendFile(__dirname + '/index.html')
+    });
+    app.post('/quotes', (req,res) => {
+        quotesCollection.insertOne(req.body)
+            .then(result => {
+                res.redirect('/')
+            })
+            .catch(error => console.error(error))
+    });
+
 })
 
-app.use(bodyParser.urlencoded({extended: true}));
 
-app.listen(3000, function() { // Creates server
-    console.log('listening on 3000')
-});
 
-app.get('/', (req,res) => {
-    res.sendFile(__dirname + '/index.html')
-});
 
-app.post('/quotes', (req,res) => {
-    console.log(req.body)
-});
+
